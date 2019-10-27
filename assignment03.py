26ec47d00bd64db3b9aa5da606aac60de08d68ce
bataisk <donnatroal@gmail.com>
2019-10-27 03:28:37 +0300

    Complete assignment

--- assignment03.py
+++ assignment03.py
@@ -5,7 +5,8 @@ def task1():
     between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence
     on a single line.
     """
-    # todo: write your code here
+
+    return ','.join(str(i) for i in range(2000, 3200) if i % 7 == 0 and i % 5 != 0)
 
 
 def task2(rows, cols):
@@ -20,7 +21,8 @@ def task2(rows, cols):
     >>> task2(3, 5)
     [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
     """
-    # todo: write your code here
+
+    return [[row*col for col in range(cols)] for row in range(rows)]
 
 
 def task3(password):
@@ -51,7 +53,12 @@ def task3(password):
     >>> task3('2We3345')
     False
     """
-    # todo: write your code here
+
+    return (6 <= len(password) <= 12
+            and any('a' <= s <= 'z' for s in password)
+            and any('A' <= s <= 'Z' for s in password)
+            and any('0' <= s <= '9' for s in password)
+            and any(s in '#$@' for s in password))
 
 
 def task4():
@@ -59,7 +66,23 @@ def task4():
     Write password generator function that uses the same rules as in Task 3.
     The password generator function must be able to generate all possible correct passwords.
     """
-    # todo: write your code here
+
+    from string import ascii_lowercase, ascii_uppercase, digits
+    from random import randint, choice, sample
+
+    all_char_sets = [ascii_lowercase, ascii_uppercase, digits, '#$@']
+    all_chars = ascii_lowercase + ascii_uppercase + digits + '#$@'
+    pas_len = randint(6, 12)
+    result = ['' for i in range(pas_len)]
+
+    for characters, index in zip(all_char_sets, sample(range(pas_len), 4)):
+        result[index] = choice(characters)
+
+    for i, char in enumerate(result):
+        if not char:
+            result[i] = choice(all_chars)
+
+    return ''.join(result)
 
 
 if __name__ == '__main__':
